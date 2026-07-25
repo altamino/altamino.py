a70094a02354f857ae8f8170b1c79eca5b1833d6
alx0rr <anon.mail.al@proton.me>
2026-07-25 09:35:16 +0300

    -

--- altamino/api/_async/__init__.py
+++ altamino/api/_async/__init__.py
@@ -2,3 +2,4 @@ from .auth import AuthModule
 from .chats import GlobalChatsModule
 from .altteam import AltTeamModule
 from .communities import GlobalCommunitiesModule
+from .users import GlobalUsersModule
